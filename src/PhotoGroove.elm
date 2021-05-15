8e2f0a02ccc91e4c9cbb5b848b63086f41a4fa49
Harry <hjwp2@cantab.net>
2021-05-15 23:20:37 +0100

    use custom types for Msg

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,5 +1,6 @@
 module PhotoGroove exposing (main)
 
+import Array exposing (Array)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -11,11 +12,19 @@ type alias Photo =
 
 
 type alias Model =
-    { photos : List Photo, selectedUrl : String }
+    { photos : List Photo, selectedUrl : String, chosenSize : ThumbnailSize }
 
 
-type alias Msg =
-    { description : String, data : String }
+type Msg
+    = ClickedPhoto String
+    | ClickedSize ThumbnailSize
+    | ClickedSurpriseMe
+
+
+type ThumbnailSize
+    = Small
+    | Medium
+    | Large
 
 
 urlPrefix : String
@@ -23,15 +32,23 @@ urlPrefix =
     "http://elm-in-action.com/"
 
 
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
+
+
 view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , button
-            [ onClick { description = "ClickedButton", data = "" } ]
+            [ onClick ClickedSurpriseMe ]
             [ text "Suprise me!" ]
+        , h3 [] [ text "Thumbnail Size:" ]
+        , div [ id "choose-size" ]
+            (List.map viewSizeChooser [ Small, Medium, Large ])
         , div
-            [ id "thumbnails" ]
+            [ id "thumbnails", class (sizeToString model.chosenSize) ]
             (List.map (viewThumbnail model.selectedUrl) model.photos)
         , img
             [ class "large", src (urlPrefix ++ "large/" ++ model.selectedUrl) ]
@@ -44,18 +61,53 @@ viewThumbnail selectedUrl thumb =
     img
         [ src (urlPrefix ++ thumb.url)
         , classList [ ( "selected", selectedUrl == thumb.url ) ]
-        , onClick { description = "ClickedPhoto", data = thumb.url }
+        , onClick (ClickedPhoto thumb.url)
         ]
         []
 
 
+viewSizeChooser : ThumbnailSize -> Html Msg
+viewSizeChooser size =
+    label []
+        [ input [ type_ "radio", name "size", onClick (ClickedSize size) ] []
+        , text (sizeToString size)
+        ]
+
+
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+    case size of
+        Small ->
+            "small"
+
+        Medium ->
+            "med"
+
+        Large ->
+            "large"
+
+
+getPhotoUrl : Int -> String
+getPhotoUrl index =
+    case Array.get index photoArray of
+        Just photo ->
+            photo.url
+
+        Nothing ->
+            ""
+
+
 update : Msg -> Model -> Model
 update msg model =
-    case msg.description of
-        "ClickedPhoto" -> { model | selectedUrl = msg.data }
-        "ClickedButton" -> { model | selectedUrl = "2.jpeg" }
-        _ -> model
+    case msg of
+        ClickedPhoto url ->
+            { model | selectedUrl = url }
 
+        ClickedSurpriseMe ->
+            { model | selectedUrl = "2.jpeg" }
+
+        ClickedSize size ->
+            { model | chosenSize = size }
 
 
 initialModel : Model
@@ -66,6 +118,7 @@ initialModel =
         , { url = "3.jpeg" }
         ]
     , selectedUrl = "1.jpeg"
+    , chosenSize = Medium
     }
 
 
