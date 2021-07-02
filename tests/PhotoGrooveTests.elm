5b0a98afa1c5041c5ca0643a127d1742db863d27
Harry <hjwp2@cantab.net>
2021-07-02 22:20:54 +0100

    first test

--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -2,10 +2,17 @@ module PhotoGrooveTests exposing (..)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
+import Json.Decode
+import PhotoGroove
 import Test exposing (..)
 
 
-suite : Test
-suite =
-    test "one plus one equals two"
-        (\_ -> Expect.equal (1 + 1) 2)
+decoderTest : Test
+decoderTest =
+    test "happy path"
+        (\_ ->
+            """ {"url": "http://fruits.com/apple.jpeg", "size": 5, "title": "this is not an apple"}"""
+                |> Json.Decode.decodeString PhotoGroove.photoDecoder
+                |> Expect.equal
+                    (Ok { url = "http://fruits.com/apple.jpeg", size = 5, title = "this is not an apple" })
+        )
