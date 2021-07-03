553743fc7e6c63e6588ed860931b0f221ee283bb
Harry <hjwp2@cantab.net>
2021-07-03 09:00:07 +0100

    use a fuzzer

--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -3,26 +3,25 @@ module PhotoGrooveTests exposing (..)
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Json.Decode
+import Json.Encode
 import PhotoGroove
 import Test exposing (..)
 
 
-decoderTests =
-    describe "decoder"
-        [ test
-            "happy path"
-          <|
-            \_ ->
-                """ {"url": "http://fruits.com/apple.jpeg", "size": 5, "title": "this is not an apple"}"""
-                    |> Json.Decode.decodeString PhotoGroove.photoDecoder
-                    |> Expect.equal
-                        (Ok { url = "http://fruits.com/apple.jpeg", size = 5, title = "this is not an apple" })
-        , test
-            "title should default to (untitled)"
-          <|
-            \_ ->
-                """ {"url": "http://fruits.com/apple.jpeg", "size": 5}"""
-                    |> Json.Decode.decodeString PhotoGroove.photoDecoder
-                    |> Expect.equal
-                        (Ok { url = "http://fruits.com/apple.jpeg", size = 5, title = "(untitled)" })
-        ]
+fuzzTest =
+    fuzz3 string int string "happy path" <|
+        \url size title ->
+            [ ( "url", Json.Encode.string url ), ( "size", Json.Encode.int size ), ( "title", Json.Encode.string title ) ]
+                |> Json.Encode.object
+                |> Json.Decode.decodeValue PhotoGroove.photoDecoder
+                |> Expect.equal
+                    (Ok { url = url, size = size, title = title })
+
+
+titleDefaultTest =
+    test "title should default to (untitled)" <|
+        \_ ->
+            """ {"url": "http://fruits.com/apple.jpeg", "size": 5}"""
+                |> Json.Decode.decodeString PhotoGroove.photoDecoder
+                |> Expect.equal
+                    (Ok { url = "http://fruits.com/apple.jpeg", size = 5, title = "(untitled)" })
