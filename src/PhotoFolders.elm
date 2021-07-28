a4a2bab66dde797822253fb2e8e884c8782e45e5
Harry <hjwp2@cantab.net>
2021-07-28 22:40:48 +0100

    more boilerplate

--- src/PhotoFolders.elm
+++ src/PhotoFolders.elm
@@ -3,6 +3,7 @@ module PhotoFolders exposing (main)
 import Browser
 import Html exposing (..)
 import Http
+import Json.Decode
 
 
 type alias Model =
@@ -22,12 +23,40 @@ view model =
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        GotInitialModel (Ok m) ->
+            let
+                _ =
+                    Debug.log "got initial model" m
+            in
+               ( m, Cmd.none )
+
+        GotInitialModel (Err e) ->
+            let
+                _ =
+                    Debug.log "error" e
+            in
+            ( model, Cmd.none )
+
+        ClickedPhoto _ ->
+            ( model, Cmd.none )
 
 
 init : flags -> ( Model, Cmd Msg )
 init _ =
-    ( { selectedPhotoUrl = Nothing }, Cmd.none )
+    let
+        initialModel =
+            { selectedPhotoUrl = Nothing }
+
+        modelDecoder : Json.Decode.Decoder Model
+        modelDecoder =
+            Json.Decode.succeed initialModel -- TODO
+
+        _ = Debug.log "init" "init"
+    in
+    ( initialModel
+    , Http.get { url = "http://elm-in-action.com/folders/list", expect = Http.expectJson GotInitialModel modelDecoder }
+    )
 
 
 main : Program () Model Msg
